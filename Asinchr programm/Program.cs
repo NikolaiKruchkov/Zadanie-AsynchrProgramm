baa890dc904312ba912702e1a0ef705c98f09cd3
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-12-13 10:23:25 +0300

    Add files via upload

---
+++ Asinchr programm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace Asinchr_programm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число");
+            uint d = Convert.ToUInt32(Console.ReadLine());
+            uint r = FactorialAsync(d).Result;
+            Console.WriteLine($"Факториал натурального числа {d} равен {r}");
+            Console.ReadKey();
+        }
+
+        public static uint Factorial(uint n)
+        {
+               uint fact=1;
+               for (uint i = 1;  i < n + 1 ; i++)
+                {
+                fact = fact * i;
+                }
+              return fact;
+
+        }
+        public static async Task<uint> FactorialAsync(uint n)
+        {
+            uint result = await Task<uint>.Run(() => Factorial(n));
+            return result;
+        }
+
+    }
+}
